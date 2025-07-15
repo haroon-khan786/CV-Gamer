0a82afb59fc0b062b0845d5a76891ca387fa2115
haroon-khan786 <musakhan0788422369@gmail.com>
2025-07-15 22:00:11 +0430

    control mouse using hand pose

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import cv2
 import mediapipe as mp
+import pyautogui
+import numpy as np
 
 # Initialize MediaPipe Hands module
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5)
 mp_drawing = mp.solutions.drawing_utils
+
+
+# Screen size
+screen_width, screen_height = pyautogui.size()
 
 # Initialize webcam capture
 cap = cv2.VideoCapture(0)
@@ -36,12 +42,20 @@ while True:
     if results.multi_hand_landmarks:
         for landmarks in results.multi_hand_landmarks:
             # Draw hand landmarks on the frame
-            mp_drawing.draw_landmarks(drawing_frame, landmarks, mp_hands.HAND_CONNECTIONS)
+            # mp_drawing.draw_landmarks(drawing_frame, landmarks, mp_hands.HAND_CONNECTIONS)
 
             # Get the position of the tip of the index finger (landmark 8)
             index_finger_tip = landmarks.landmark[8]
             finger_x = int(index_finger_tip.x * frame.shape[1])  # Normalize x to pixel
             finger_y = int(index_finger_tip.y * frame.shape[0])  # Normalize y to pixel
+
+            # Convert to screen coordinates
+            screen_x = np.interp(index_finger_tip.x, [0, 1], [0, screen_width])
+            screen_y = np.interp(index_finger_tip.y, [0, 1], [0, screen_height])
+
+             # Move mouse
+            pyautogui.moveTo(screen_x, screen_y)
+
 
             # Draw a circle on the frame at the finger's position (red circle)
             cv2.circle(drawing_frame, (finger_x, finger_y), 10, (0, 0, 255), -1)  # Red circle
